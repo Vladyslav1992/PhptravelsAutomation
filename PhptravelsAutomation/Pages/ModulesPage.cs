d5dd9aa8cfa3bc23c7607c70b681be91f1ab73c1
Vladyslav1992 <vladpleten@gmail.com>
2019-04-15 12:07:32 +0300

    add ModulesPage

--- PhptravelsAutomation/Pages/ModulesPage.cs
+++ PhptravelsAutomation/Pages/ModulesPage.cs
@@ -1,8 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace PhptravelsAutomation.Pages
 {
     class ModulesPage
     {
+        [FindsBy(How = How.CssSelector, Using = "a[href$='.net']")]
+        public IWebElement HomePageLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//div[contains(., 'user') and @class='col-md-8']")]
+        public IWebElement UserCredentials { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "a[href$='admin']")]
+        public IWebElement AdminPageLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//div[contains(., 'admin') and @class='col-md-8']")]
+        public IWebElement AdminCredentials { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "a[href$='.supplier']")]
+        public IWebElement SupplierPageLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "div[contains(., 'supplier') and @class='col-md-8']")]
+        public IWebElement SupplierCredentials { get; set; }
     }
 }
