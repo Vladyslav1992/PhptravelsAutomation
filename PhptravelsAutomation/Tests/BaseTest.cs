374a9bf81f33e7581dd950c94513bf131c9dfdbf
Vladyslav1992 <vladpleten@gmail.com>
2019-04-15 15:38:39 +0300

    add BaseTest

---
+++ PhptravelsAutomation/Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PhptravelsAutomation.Tests
+{
+    class BaseTest
+    {
+        public IWebDriver Driver { get; set; } = new ChromeDriver();
+
+        [OneTimeSetUp]
+        public void GeneralSetUp()
+        {
+            Driver.Navigate().GoToUrl("https://www.phptravels.net/account/");
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void Clearup()
+        {
+            Driver.Quit();
+        }
+    }
+}
