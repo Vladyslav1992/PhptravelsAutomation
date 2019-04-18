43fba94e464d1729c4e9cd626b3e06f14d2a3bc4
Vladyslav1992 <vladpleten@gmail.com>
2019-04-18 19:01:28 +0300

    add LoginPage

---
+++ PhptravelsAutomation/Pages/LoginPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PhptravelsAutomation.Pages
+{
+    class LoginPage : BasePage
+    {
+        [FindsBy(How = How.Name, Using = "email")]
+        public IWebElement EmailField { get; set; }
+
+        [FindsBy(How = How.Name, Using = "password")]
+        public IWebElement PasswordField { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "btn-lg")]
+        public IWebElement LoginButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='alert alert-danger']")]
+        public IWebElement ErrorAlert { get; set; }
+    }
+}
